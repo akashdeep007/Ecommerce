a237002d5d481fe1aa94d49d9896e3496a0f20e7
akashdeep007 <beingakscool@gmail.com>
2020-07-06 00:17:36 +0530

    password hashed using bcrypt

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,62 +1,71 @@
-import express from 'express';
+import express from "express";
+const bcrypt = require("bcryptjs");
 import User from "../models/userModel";
-import { getToken } from '../util';
+import { getToken } from "../util";
 
 const router = express.Router();
 
-router.post("/signin", async (req, res)=> {
-    const signinUser =  await User.findOne({
-        email: req.body.email,
-        password: req.body.password
-    });
-    if(signinUser){
-        res.send({
-            _id: signinUser.id,
-            name: signinUser.name,
-            email: signinUser.email,
-            isAdmin: signinUser.isAdmin,
-            token: getToken(signinUser)
-        })
-    }
-    else{
-        res.status(401).send({msg: "Invalid Email and password"});
-    }
-})
-
-router.post('/register', async (req, res) => {
-    const user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-    });
-    const newUser = await user.save();
-    if (newUser) {
-        res.send({
-            _id: newUser.id,
-            name: newUser.name,
-            email: newUser.email,
-            isAdmin: newUser.isAdmin,
-            token: getToken(newUser),
-        });
+router.post("/signin", async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const signinUser = await User.findOne({
+    email: email,
+  });
+  if (signinUser) {
+    const passisequal = await bcrypt.compare(password, signinUser.password);
+    // console.log(passisequal);
+    if (passisequal) {
+      res.send({
+        _id: signinUser.id,
+        name: signinUser.name,
+        email: signinUser.email,
+        isAdmin: signinUser.isAdmin,
+        token: getToken(signinUser),
+      });
     } else {
-        res.status(401).send({ message: 'Invalid User Data.' });
+      res.status(401).send({ msg: "Wrong Password" });
     }
+  } else {
+    res.status(401).send({ msg: "Invalid Email" });
+  }
+});
+
+router.post("/register", async (req, res) => {
+  const password = req.body.password;
+  const hashedPw = await bcrypt.hash(password, 12);
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: hashedPw,
+  });
+  const newUser = await user.save();
+  if (newUser) {
+    res.send({
+      _id: newUser.id,
+      name: newUser.name,
+      email: newUser.email,
+      isAdmin: newUser.isAdmin,
+      token: getToken(newUser),
+    });
+  } else {
+    res.status(401).send({ message: "Invalid User Data." });
+  }
 });
 
 router.get("/createadmin", async (req, res) => {
-    try {
-        const user = new User({
-            name: 'Satyaki',
-            email: "sat@ki.com",
-            password: "12345",
-            isAdmin: true
-        })
+  try {
+    const user = new User({
+      name: "Satyaki",
+      email: "sat@ki.com",
+      password: "12345",
+      isAdmin: true,
+    });
 
-        const newUser = await user.save();
-        res.send(newUser);
-    } catch (error) {
-        res.send({msg: error.message});
-    }
-})
+    const newUser = await user.save();
+    res.send(newUser);
+  } catch (error) {
+    res.send({ msg: error.message });
+  }
+});
 
 export default router;
